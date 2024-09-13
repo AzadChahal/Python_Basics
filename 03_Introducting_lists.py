b92f9ebab62bcf77dabad0fab952e8e2d42051f3
Azadwinder Chahal <azadchahalpau@gmail.com>
2024-09-12 21:50:46 -0400

    Changes in List sept

---
+++ 03_Introducting_lists.py
@@ -0,0 +1,31 @@
+#%%
+#Introducing List
+    # What is a list?
+    # Changing, Adding, Removing Elements
+    # Organizing a List
+    # Avoiding Index Errors When Working with Lists
+
+# %%
+
+# Types in Python
+x = x = 5
+s = "geeksforgeeks"
+y = [1,2,3]
+print(type(x))
+print(type(s))
+print(type(y))
+
+# %%
+
+# What is list? A list is a collection of items in particular order. 
+# List could include letters of the alphabets, the digit from 0-9, or name of all the people in your family.
+# Because multiple items are in a list, so keeping the names plural is a good practice
+
+# Square bracket [] indicate a list
+# individual element in a list is separated by commas. 
+
+bicycles = ["trek", "cannodale", "redline", "specialized"]
+print(bicycles)
+
+
+# %%
